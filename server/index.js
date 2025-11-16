c2a3f74c1b76a8a280eb8f583cd84a85ed7b00bf
Gare Ewefada <gewefada@gmail.com>
2025-11-15 21:08:02 -0600

    initial commit. front and backend scaffold

---
+++ server/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Resonate API running");
+});
+
+// placeholder for scraper route
+app.post("/parse", async (req, res) => {
+  const { url } = req.body;
+  res.json({ message: "Parser not built yet", url });
+});
+
+app.listen(5000, () => {
+  console.log("Server running on http://localhost:5000");
+});
